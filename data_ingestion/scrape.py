d32f803664131ae51f81f388378bf5eb3701f2e6
blakeban <blakedismore@gmail.com>
2026-01-15 18:55:12 -0600

    Update scrape.py
    Downloads per lap telemetry for each driver in a specific race.

--- data_ingestion/scrape.py
+++ data_ingestion/scrape.py
@@ -1,8 +1,10 @@
 import fastf1
 import requests
 import logging
+from urllib.parse import quote
+import os
 
-base_url = 'https://github.com/TracingInsights-Archive/2025/tree/main/'
+base_url = 'https://raw.githubusercontent.com/TracingInsights-Archive/2025/main/'
 end_url = '/Race'
 
 file_extension = '_tel.json'
@@ -16,27 +18,34 @@ def main():
 
     drivers_list = []
     driver_dict = {}
-
+    
     laps_dict = {}
 
     for driver in session.drivers:
         driver_dict[driver] = session.get_driver(driver)['Abbreviation']
         drivers_list.append(session.get_driver(driver)['Abbreviation'])
 
-    url_event_name = event_name.replace(' ', '%20')
-
+    url_event_name = quote(event_name, safe='')
     url = base_url + url_event_name + end_url
-    
+
     for driver in driver_dict.keys():
         laps_dict[driver] = int(session.results['Laps'][driver] + 0.99)
-    
+
     for driver in driver_dict.keys():
-        
-        addition_url = ''
-        ## download url + addition_url + file_extension using requests
-        pass
-    
-    print(laps_dict)
+        abbr = driver_dict[driver]
+
+        for lap in range(1, laps_dict[driver] + 1):
+            addition_url = f'/{abbr}/{lap}{file_extension}'
+            download_url = url + addition_url
+
+            print(abbr, lap)
+            r = requests.get(download_url)
+            if r.status_code == 200:
+                path = f'telemetry/{event_name}/Race/{abbr}'
+                os.makedirs(path, exist_ok=True)
+                with open(f'{path}/{lap}{file_extension}', 'wb') as f:
+                    f.write(r.content)
+
 
 if __name__ == '__main__':
     main()
