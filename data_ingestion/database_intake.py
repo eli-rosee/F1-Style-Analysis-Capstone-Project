a4ef13bca7c5f599f9fb8ffb93066a1225951339
eli-rosee <eli.j.rose.04@gmail.com>
2026-02-09 18:59:07 +0000

    added some test code for connecting to database

--- data_ingestion/database_intake.py
+++ data_ingestion/database_intake.py
@@ -1,4 +1,5 @@
 import psycopg2
+from psycopg2 import Error
 
 def connect_to_db():
     # Connect to your postgres DB
@@ -6,14 +7,30 @@ def connect_to_db():
         host="localhost",
         database="postgres",
         user="postgres",
-        password="postgres"
+        password="Team6"
     )
     return conn
 
 def main():
-    conn = connect_to_db()
-    print("Connected to the database successfully!")
-    conn.close()
+    try:
+        conn = connect_to_db()
+        print("Connected to the database successfully!")
+        cursor = conn.cursor()
+        print("PostgreSQL server information")
+        print(conn.get_dsn_parameters(), "\n")
+        # Executing a SQL query
+        cursor.execute("SELECT version();")
+        # Fetch result
+        record = cursor.fetchone()
+        print("You are connected to - ", record, "\n")
+
+    except (Exception, Error) as e:
+        print(f"Error connecting to the database: {e}")
+    finally:
+        if(conn):
+            conn.close()
+            cursor.close()
+            print("PostgreSQL connection is closed")
 
 if __name__ == "__main__":
     main()
