22533ee4040d500f35095d6a00209ec22c957e00
eli-rosee <eli.j.rose.04@gmail.com>
2026-02-09 18:40:37 +0000

    basic code added for database conn

--- data_ingestion/database_intake.py
+++ data_ingestion/database_intake.py
@@ -1,6 +1,19 @@
+import psycopg2
+
+def connect_to_db():
+    # Connect to your postgres DB
+    conn = psycopg2.connect(
+        host="localhost",
+        database="postgres",
+        user="postgres",
+        password="postgres"
+    )
+    return conn
 
 def main():
-    pass
+    conn = connect_to_db()
+    print("Connected to the database successfully!")
+    conn.close()
 
 if __name__ == "__main__":
     main()
